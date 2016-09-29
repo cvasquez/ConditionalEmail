e3cd22871f5c9ad9af1d03a75715e621a74ec887
Chris Vasquez <christv@aweber.com>
2016-09-28 22:51:11 -0400

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,52 @@
+var snippet,
+    variable,
+    condition = '',
+    htmlContent = $('.htmlContent'),
+    variable_val = $('.variable'),
+    custom_val = $('.custom_field input'),
+    cond_val = $('.condition'),
+    eq_val = $('.condition_value input');
+
+// Show input when custom field is selected
+$('.variable').on('change', function(){
+  if($(this).val() == 'custom') {
+    $('.custom_field').css('display', 'inline-block');
+  } else {
+    $('.custom_field').css('display', 'none');
+  }
+});
+
+// Show input when equals string or equals number is selected
+$('.condition').on('change', function(){
+  if($(this).val() == 'eq_string' || $(this).val() == 'eq_num') {
+    $('.condition_value').css('display', 'inline-block');
+  } else {
+    $('.condition_value').css('display', 'none');
+  }
+});
+
+// Build Snippet Code
+function buildSnippet(){
+
+  // If custom variable is selected
+  if(variable_val.val() == 'custom') {
+    variable = 'custom ' + custom_val.val();
+
+  // If standard personalization is selected
+  } else if (variable_val.val() != 'custom') {
+    variable = variable_val.val();
+  }
+
+  // If equals numberif
+  if(cond_val.val() == 'eq_string') {
+    condition = '==\'' + eq_val.val() + '\'';
+  } else if (cond_val.val() == 'eq_num') {
+    condition = '==' + eq_val.val();
+  } else {
+    condition = '';
+  }
+
+  // Build snippet
+  snippet = '{% if globals().get(\'' + variable + '\')' + condition + ' %}' + htmlContent.val() + '{% end %}'
+  console.log(snippet);
+}
